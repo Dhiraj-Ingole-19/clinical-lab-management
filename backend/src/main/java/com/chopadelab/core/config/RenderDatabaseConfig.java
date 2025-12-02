2ea0836b18ab3e245c44bdfa46681d4942238425
Dhiraj-Ingole-19 <dhirajingole19@gmail.com>
2025-12-02 16:58:35 +0530

    fix: add RenderDatabaseConfig to parse DATABASE_URL

---
+++ backend/src/main/java/com/chopadelab/core/config/RenderDatabaseConfig.java
@@ -0,0 +1,32 @@
+package com.chopadelab.core.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class RenderDatabaseConfig {
+
+    @Bean
+    @ConditionalOnExpression("!'${DATABASE_URL:}'.isEmpty()")
+    public DataSource dataSource() throws URISyntaxException {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
